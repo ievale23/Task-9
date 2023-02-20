cfe2f105e8616a023fa76d54fae277b1aa6e7647
ievale23 <ieva.lebedyte@gmail.com>
2023-02-20 21:26:32 +0200

    šį bei tą turim, kažkas neveikia

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 /* ------------------------------ TASK 9 ---------------------------------------------------------------
-Sukurkite konstruktoriaus funkciją "Movie" (naudokte ES6), kuri sukuria objektus su 3 savybėm ir 1 metodu:
+Sukurkite konstruktoriaus funkciją "Movie" (naudokte ES6), 
+kuri sukuria objektus su 3 savybėm ir 1 metodu:
 
 Savybės:
 title, director, budget
 Metodas: 
-wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kitu atveju false 
+wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, 
+tada gražins true, kitu atveju false 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+
+  wasExpensive() {
+    if (this.budget > 100000000) {
+      console.log("movie successful");
+    }
+  }
+}
+
+const movie1 = new Movie(
+  "Everything Everywhere All at Once",
+  "Daniel Kwan, Daniel Scheinert",
+  107000000
+);
